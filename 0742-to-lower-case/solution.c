3ac982a813a1c27e8472288b6ca1fa0d3e853cd3
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2024-08-02 15:50:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (49.09%)

--- 0742-to-lower-case/solution.c
+++ 0742-to-lower-case/solution.c
@@ -1,7 +1,7 @@
 char* toLowerCase(char* s) {
     int len =strlen(s);
     for(int i=0;i<len;i++){
-        if(s[i]>=65 && s[i]<91)
+        if(s[i]>=65 && s[i]<=90)
         s[i] = s[i] + 32;  
     }
     return s;
