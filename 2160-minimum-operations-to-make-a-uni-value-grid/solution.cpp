6c59dbb830801af6908c1ed74dc8cec3540b5bf7
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2025-03-26 03:34:50 +0000

    Sync LeetCode submission Runtime - 38 ms (71.19%), Memory - 87.1 MB (76.54%)

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+      vector<int> array;
+      int remainder = grid[0][0]%x;
+      for(int i=0; i<grid.size(); i++){
+        for(int j=0;j<grid[0].size();j++){
+            if(grid[i][j]%x != remainder) return -1;
+            array.push_back(grid[i][j]);
+        }
+      }
+      sort(array.begin(),array.end());
+      int n = array.size();
+      int median = n/2;
+
+      int count = 0;
+      for(int i=0;i<n;i++){
+        count += abs(array[i] -array[median])/x;
+      }  
+      return count;
+    }
+};
