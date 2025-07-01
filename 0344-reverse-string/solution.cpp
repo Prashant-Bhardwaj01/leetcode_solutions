4d4c307eebda645c7abc72ee8083262dcd0d2b23
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2025-07-01 06:17:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.1 MB (80.21%)

---
+++ 0344-reverse-string/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0;
+        int j=s.size() - 1;
+        while(i!=j && i<j && j>i){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+    }
+};
