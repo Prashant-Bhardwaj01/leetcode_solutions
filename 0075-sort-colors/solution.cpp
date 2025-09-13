121b66e9bea16905dc64b9efa5f46759a3b20a81
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2025-09-13 17:06:06 +0000

    Sync LeetCode submission Runtime - 3 ms (2.71%), Memory - 11.6 MB (77.92%)

--- 0075-sort-colors/solution.cpp
+++ 0075-sort-colors/solution.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+    void sortColors(vector<int>& arr) {
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j] > arr[j+1]) swap(arr[j],arr[j+1]);
+            }
+        }
     }
 };
