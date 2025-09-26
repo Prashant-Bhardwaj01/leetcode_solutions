ffcd3bab509ba97223092b7743f1b05e0c75cf71
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2025-09-26 17:06:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.2 MB (66.90%)

---
+++ 0083-remove-duplicates-from-sorted-list/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* helper = head;
+        while(helper && helper->next){
+            if(helper->val == helper->next->val)helper->next = helper->next->next;
+            else helper=helper->next;
+        }
+        return head;
+    }
+};
