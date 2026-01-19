ca4b593e2e6bab7fd11919ef44c00028762fab84
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-01-19 13:07:29 +0000

    Sync LeetCode submission Runtime - 2 ms (28.24%), Memory - 52.3 MB (56.77%)

---
+++ 0055-jump-game/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxx = 0;
+        for(int i=0;i<n;i++){
+            if(i>maxx) return false;
+            maxx = max(maxx,i+nums[i]);
+        }
+        return true;
+    }
+};
