85cd119e67b8056185730ccea732a67bfa1c0be1
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-01-29 04:50:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.5 MB (7.39%)

--- 0074-search-a-2d-matrix/solution.cpp
+++ 0074-search-a-2d-matrix/solution.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>& mat, int target, int row){
-        int n = mat[0].size();
-        int st = 0;
-        int end = n-1;
-        while(st<=end){
-            int mid = st + (end -st)/2;
-            if(mat[row][mid] == target) return true;
-            else if(target > mat[row][mid]) st = mid + 1;
-            else end = mid - 1;
-        }
-        return false;
-    }
-    bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int n = mat.size();
-        int m = mat[0].size();
-        int st = 0;
-        int end = n-1;
-        while(st<=end){
-            int mid = st + (end -st) /2;
-            if(target >= mat[mid][0] && target <= mat[mid][m-1]) return check(mat,target,mid);
-            else if(target >= mat[mid][m-1]) st = mid + 1;
-            else end = mid - 1;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int l = 0, h = n - 1;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if (matrix[mid][0] <= target && matrix[mid][m - 1] >= target) {
+                int start = 0, end = m - 1;
+
+                while (start <= end) {
+                    int midCol = start + (end - start) / 2;
+                    if (matrix[mid][midCol] == target) return true;
+                    else if (matrix[mid][midCol] > target) end = midCol - 1;
+                    else start = midCol + 1;
+                }
+                return false;
+            }
+            else if (matrix[mid][m - 1] > target) {
+                h = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
         }
         return false;
     }
 };
+
