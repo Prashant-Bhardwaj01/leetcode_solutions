f4e0dfbad7ccde3a5b72e3040f3df33d4a6e8ec4
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-02-11 14:34:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.5 MB (77.92%)

--- 0075-sort-colors/solution.cpp
+++ 0075-sort-colors/solution.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
-    void sortColors(vector<int>& arr) {
-        int n = arr.size();
+    void sortColors(vector<int>& nums) {
+        // int l = 0;
+        // int r = nums.size()-1;
+        // while(l<r){
+        //     if(nums[l]<nums[r]){
+        //         l++;
+        //     }
+        //     else if(nums[l]>nums[r]){
+        //         swap(nums[l],nums[r]);
+        //         r--;
+        //     }else{
+        //         l++;
+        //         r--;
+        //     }
+        // }
+        int n = nums.size();
+        
+        int z = 0;
+        int f = 0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(arr[j] > arr[j+1]) swap(arr[j],arr[j+1]);
-            }
+            if( nums[i] == 0) z++;
+            else if( nums[i] == 1)f++;
         }
+        for(int i =0;i<n;i++){
+            if(i<z) nums[i] = 0;
+            else if(i<f+z) nums[i] = 1;
+            else nums[i] = 2;
+        }
+        
     }
 };
