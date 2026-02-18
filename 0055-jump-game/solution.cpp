fcfbe6ca24cd83ab09daa7db04c5b2a8bc5deffc
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-02-18 08:59:31 +0000

    Sync LeetCode submission Runtime - 624 ms (5.56%), Memory - 55.7 MB (6.27%)

--- 0055-jump-game/solution.cpp
+++ 0055-jump-game/solution.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int maxx = 0;
-        for(int i=0;i<n;i++){
-            if(i>maxx) return false;
-            maxx = max(maxx,i+nums[i]);
+    bool dfs(vector<int>& nums,int idx,vector<int>& dp){
+        if(idx == nums.size()-1) return true;
+        if(dp[idx] !=-1) return dp[idx];
+        for(int jump = 1;jump<=nums[idx];jump++){
+            if(dfs(nums,idx+jump,dp)) return dp[idx+jump] = 1;
         }
-        return true;
+        return dp[idx] =0;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,-1);
+
+        return dfs(nums,0,dp);
     }
 };
+
