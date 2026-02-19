469ef5f43cda1054d9d8392a4af55a700acfae5a
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-02-19 05:05:29 +0000

    Sync LeetCode submission Runtime - 1 ms (9.05%), Memory - 10.8 MB (21.33%)

--- 0198-house-robber/solution.cpp
+++ 0198-house-robber/solution.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    int can(int i,vector<int>& nums,vector<int>& dp){
-        if(i>=nums.size()) return 0;
+    int dfs(vector<int>& nums,int i, vector<int>& dp){
+        if(i<0) return 0;
         if(dp[i] != -1) return dp[i];
-        int take = nums[i] + can(i+2,nums,dp);
-        int skip = can(i+1,nums,dp);
-        return dp[i] = max(take,skip);
+        int l = nums[i] + dfs(nums,i-2,dp);
+        int r = 0 + dfs(nums,i-1,dp);
+        return dp[i] = max(l,r);
+    }
+    int dfs_tab(vector<int>& nums,vector<int>&  dp,int n){
+        for(int i=0;i<n;i++){
+            if(i==0){
+                dp[i] = nums[0];
+                continue;
+            }
+            int l = nums[i] ;
+            int r = 0;
+            if(i-2>=0)
+            l += dp[i-2];
+            if(i-1>=0)
+            r = 0 + dp[i-1];
+            dp[i] = max(l,r);
+        }
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,-1);
-        return can(0,nums,dp);
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        // return dfs(nums,n-1,dp);
+        return dfs_tab(nums,dp,n);
     }
 };
