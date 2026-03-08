daada887321066b6cc1d2084af94f573551c64b8
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-03-08 17:08:27 +0000

    Sync LeetCode submission Runtime - 18 ms (16.03%), Memory - 40.5 MB (5.82%)

--- 2090-number-of-ways-to-arrive-at-destination/solution.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/solution.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
+    const int MOD = 1e9+7;
     int countPaths(int n, vector<vector<int>>& roads) {
-        const int MOD = 1e9 + 7;
-        vector<vector<pair<int,int>>> graph(n);
-        for(auto& road : roads){
-            graph[road[0]].push_back({road[1],road[2]});
-            graph[road[1]].push_back({road[0],road[2]});
+        vector<vector<pair<long,long>>> adjList(n);
+        for(auto i: roads){
+            int u = i[0];
+            int v = i[1];
+            int w = i[2];
+            adjList[u].push_back({v,w});
+            adjList[v].push_back({u,w});
         }
-        vector< long long > dist(n,LLONG_MAX);
-        vector<int> ways(n,0);
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
-        dist[0] = 0;
-        ways[0] = 1;
+
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> pq;
+        vector<long long> dist(n,LLONG_MAX);
+        vector<long long> count(n,0);
+        count[0] = 1;
+
+        dist[0]= 0;
         pq.push({0,0});
         while(!pq.empty()){
-            auto [d,node] = pq.top();
+            pair<long long ,int> top = pq.top();
+            long long wt = top.first;
+            int node = top.second;
             pq.pop();
-            if(d > dist[node]) continue;
-            for(auto [neighbor ,weight] : graph[node]){
-                long long nd = d+weight;
-                if(nd < dist[neighbor]){
-                    dist[neighbor] = nd;
-                    ways[neighbor] = ways[node];
-                    pq.push({nd, neighbor});
-                }else if(nd == dist[neighbor]){
-                    ways[neighbor] = (ways[neighbor] + ways[node]) % MOD;
+            if(dist[node]<wt) continue;
+            for(auto nbr: adjList[node]){
+                int nbr_node = nbr.first;
+                int nbr_wt = nbr.second;
+                if(dist[nbr_node] > wt+nbr_wt){
+                    dist[nbr_node] = (wt+nbr_wt);
+                    count[nbr_node] = count[node];
+                    pq.push({dist[nbr_node], nbr_node});
+                }else if(wt+nbr_wt==dist[nbr_node]){
+                    count[nbr_node] = (count[nbr_node]+count[node])%MOD;
                 }
             }
         }
-        return ways[n - 1];
+        return count[n-1];
     }
-}; 
+};
