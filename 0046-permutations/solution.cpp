400c51a385d6497e1971bcc50260aa5f2ce3d608
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-03-14 17:39:39 +0000

    Sync LeetCode submission Runtime - 4 ms (11.60%), Memory - 12.1 MB (10.15%)

--- 0046-permutations/solution.cpp
+++ 0046-permutations/solution.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    void dfs(int i,int n,vector<int>& nums, vector<vector<int>>& ans){
-        if(i>=n){
-            ans.push_back(nums);
-            return ;
+    void dfs(int i, vector<int>& nums, vector<vector<int>>& ans, vector<int> sub, vector<bool>& vis){
+        if(sub.size() == nums.size()){
+            ans.push_back(sub);
+            return;
         }
-        for(int s=i;s<n;s++){
-            swap(nums[s],nums[i]);
-            dfs(i+1,n,nums,ans);
-            swap(nums[i],nums[s]);
+        for(int x=0; x<nums.size();x++){
+            if(vis[x]) continue;
+            vis[x] = true;
+            sub.push_back(nums[x]);
+        
+            dfs(x+1, nums, ans, sub, vis);
+            sub.pop_back();
+            vis[x] = false;
+            
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
         vector<vector<int>> ans;
-        dfs(0,n,nums,ans);
+        vector<int> sub;
+        vector<bool> vis(nums.size(), false);
+        dfs(0, nums, ans, sub, vis);
         return ans;
     }
 };
