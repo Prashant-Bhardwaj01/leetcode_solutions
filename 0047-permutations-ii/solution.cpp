40b210d75c3da9f4115d6cf350278bb7dc3bd7cd
Prashant-Bhardwaj01 <148607757+Prashant-Bhardwaj01@users.noreply.github.com>
2026-03-15 15:22:05 +0000

    Sync LeetCode submission Runtime - 11 ms (19.25%), Memory - 15 MB (21.22%)

--- 0047-permutations-ii/solution.cpp
+++ 0047-permutations-ii/solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void per(vector<int>& nums,int idx, vector<vector<int>>& ans, unordered_set<string>& vis){
-        if(idx==nums.size()){
-            string s;
-            for(int i: nums) s+=to_string(i)+',';
-            if(!vis.count(s)){
-                vis.insert(s);
-                ans.push_back(nums);
-            }
-            return ;
-        } 
-        for(int i=idx;i<nums.size();i++){
+    int n ;
+    void dfs(int idx, vector<int>& nums, vector<vector<int>>& ans){
+        if(idx == n){
+            ans.push_back(nums);
+            return;
+        }
+        unordered_set<int> st;
+        for(int i=idx; i<n; i++){
+            if(st.find(nums[i]) != st.end())continue;
+            st.insert(nums[i]);
             swap(nums[i],nums[idx]);
-            per(nums,idx+1,ans,vis);
-            swap(nums[i],nums[idx]);
+            dfs(idx+1, nums, ans);
+            swap(nums[idx],nums[i]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        n = nums.size();
         vector<vector<int>> ans;
-        unordered_set<string> vis;
-        per(nums,0,ans,vis);
+        
+        dfs(0,nums,ans);
         return ans;
     }
 };
